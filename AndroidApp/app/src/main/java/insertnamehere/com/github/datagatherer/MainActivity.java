d762876eb3fe3ae699d1942c1adaae92dd9af63d
mviktor02 <82106070+mviktor02@users.noreply.github.com>
2022-03-20 18:30:03 +0100

    feat(AndroidApp): created the Android App for data gathering

---
+++ AndroidApp/app/src/main/java/insertnamehere/com/github/datagatherer/MainActivity.java
@@ -0,0 +1,14 @@
+package insertnamehere.com.github.datagatherer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
