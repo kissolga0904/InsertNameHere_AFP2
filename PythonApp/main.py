da3ac0e30ad421cb71b47ddc736a9629c5e6f3fd
kissolga0904 <qlcbh6@std.uni-eszterhazy.hu>
2022-04-01 17:47:36 +0200

    feat(PythonApp): showing sensor data on graphs

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,16 +1,11 @@
-from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.slider import Slider
 from kivymd.app import MDApp
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 import matplotlib.pyplot as plt
 import json
-from kivy.core.window import Window
 
 class Manager(ScreenManager):
     pass
@@ -40,8 +35,62 @@ class ChooseFileScreen(Screen):
             self.manager.current = "choose_graph_screen"
 
 class ChooseGraphScreen(Screen):
+    def __init__(self,**kw):
+        super().__init__(**kw)
+        self.dict = None
+
     def load(self, file):
-        pass
+        with open(file) as f:
+            self.dict = json.load(f)
+
+    def show_graph(self,graph_type:str):
+        if graph_type == "accelerometer" or graph_type == "gyroscope" or graph_type == "magnetometer":
+            time_array = self.dict[graph_type + "_timestamps"]
+            time_zero = time_array[0]
+            time = [t - time_zero for t in time_array]
+            values = self.dict[graph_type]
+            x = [float(s.split(";")[0]) for s in values]
+            y = [float(s.split(";")[1]) for s in values]
+            z = [float(s.split(";")[2]) for s in values]
+            self.manager.get_screen("graph_screen").load_xyz(graph_type,time,x,y,z)
+        self.manager.current = "graph_screen"
+
+class GraphScreen(Screen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.graph_type = ""
+
+    def load_xyz(self, graph_type:str, time, x, y, z):
+        self.graph_type = graph_type
+        plt.close("all")
+        plt.figure(0)
+        plt.cla()
+        box = self.ids.graph
+        box.clear_widgets()
+        plt.plot(time, x, label="x")
+        plt.plot(time, y, label="y")
+        plt.plot(time, z, label="z")
+        plt.legend()
+        plt.grid()
+        plt.title(graph_type.replace("_"," ").title())
+        plt.xlabel("Time")
+        switcher = {
+            "accelerometer": "Acceleration (m/s²)",
+            "gyroscope": "Rotation rate (rad/s)",
+            "magnetometer": "Magnetic field (μT)"
+        }
+        plt.ylabel(switcher.get(graph_type))
+        box.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+
+    def export(self):
+        plt.savefig(self.graph_type)
+        popup = Popup(
+            title="Successfully export " + self.graph_type,
+            content = BoxLayout(),
+            size_hint = (.4,.2)
+        )
+        popup.open()
+
 
 class MainApp(MDApp):
     def build(self):
