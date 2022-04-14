7d4520fd92da129e82a8ba3b45e5a926a62fc50b
mviktor02 <82106070+mviktor02@users.noreply.github.com>
2022-04-14 03:13:25 +0200

    feat(AndroidApp): basic GraphSelectorActivity

---
+++ AndroidApp/app/src/main/java/insertnamehere/com/github/datagatherer/activities/GraphSelectorActivity.java
@@ -0,0 +1,20 @@
+package insertnamehere.com.github.datagatherer.activities;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import insertnamehere.com.github.datagatherer.R;
+
+public class GraphSelectorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LinearLayout layout = findViewById(R.id.linearLayout);
+        // TODO add buttons programatically with LinearLayout#addView
+    }
+}
